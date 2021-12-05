6caa335b250a1ddacc9d3396acd864e05d6d0052
Магди Османов <magdi.osmanov97@gmail.com>
2021-12-05 21:44:26 +0300

    Добавьте файлы проекта.

---
+++ Ex.9/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            
+            try
+            {
+                Console.WriteLine("Введите первое число");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("код операции (например, 1 – сложение, 2 – вычитание, 3 – произведение, 4 – частное).");
+                char k = Convert.ToChar(Console.ReadLine());
+                switch (k)
+                {
+                    case '1':
+                        answer = a + b;
+                        Console.WriteLine("Ответ:",answer);
+                        break;
+                    case '2':
+                        answer = a - b;
+                        Console.WriteLine("Ответ:", answer);
+                        break;
+                    case '3':
+                        answer = a * b;
+                        Console.WriteLine("Ответ:", answer);
+                        break;
+                    case '4':
+                        try
+                        {
+                            answer = a/b;
+                            Console.WriteLine("Ответ:", answer);
+                        }
+                        catch (DivideByZeroException) 
+                        {
+                            Console.WriteLine("Деление на ноль");
+                        }
+                        break;
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("Неверный тип данных");
+            }
+            Console.ReadKey();
+        }
+    }
+}
